1b1b64e2d327c54e863fc455373921a53ce95a6a
larry leow <leow.pauwan@gmail.com>
2017-08-19 02:45:36 +0800

    script file prototype

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,4 @@
-function add10() {
-    var elem = document.getElementById("myBar"); 
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++; 
-            elem.style.width = width + '%'; 
-        }
-    }
+function alertTest()
+{
+	alert("clicked");
 }
