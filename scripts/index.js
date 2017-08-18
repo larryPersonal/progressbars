4aeba6bfe122a9ab0c018a6112666e44eb639126
larry leow <leow.pauwan@gmail.com>
2017-08-19 02:30:53 +0800

    first commit: UI only

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+function add10() {
+    var elem = document.getElementById("myBar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
